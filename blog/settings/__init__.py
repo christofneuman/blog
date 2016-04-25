ad06beaefb0b21a2ddf9f5663a9fd069dd8f9eb5
Christof Neuman <christof@namespace.ee>
2016-04-25 21:02:32 +0300

    dream consttlations addition

--- blog/settings/__init__.py
+++ blog/settings/__init__.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'redactor',
-    'widget_tweaks',
+    # 'widget_tweaks',
 
     'blog',
 )
--- blog/settings/production.py
+++ blog/settings/production.py
@@ -10,10 +10,10 @@ DATABASES = {
 }
 
 # E-mail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mandrillapp.com'
-EMAIL_HOST_USER = 'christof@namespace.ee'
-EMAIL_HOST_PASSWORD = 'qSqMN_ZdtE5a1LbHLyGlSw'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# # EMAIL_HOST = 'smtp.mandrillapp.com'
+# # EMAIL_HOST_USER = 'christof@namespace.ee'
+# # EMAIL_HOST_PASSWORD = 'qSqMN_ZdtE5a1LbHLyGlSw'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
